1b0c6385ebd7118090bbd3d4eef0c8e2e58726c7
Shadow_YX <57027996+Shadow-YX@users.noreply.github.com>
2021-09-23 02:49:25 +0800

    Add files via upload

---
+++ AsmX32Debug/link.asm
@@ -0,0 +1,22 @@
+.586
+.model flat,stdcall
+option casemap:none
+
+    include link.inc
+    include msvcrt.inc
+    include windows.inc
+    includelib msvcrt.lib
+
+.code 
+
+
+
+
+end
+
+
+
+
+
+
+
